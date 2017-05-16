95359fd6377fda49d035ba24d7587a25614c379c
Ben Hays <ben@kajabi.com>
2017-05-16 10:10:33 -0700

    Git circles to animate independent of each other.

--- source/javascripts/kajabi/circle.js
+++ source/javascripts/kajabi/circle.js
@@ -2,26 +2,21 @@
 
   console.log("rad!");
 
-  var $circle = $(".circle");
-  var $circleSize = $(".circle__size");
-  var $circleMove = $(".circle__move");
-
-  var updatedValues;
-
   // Scaling
   var scaleTime;
   var scaleTimeRange = 1000;
   var scaleAmount;
   var scaleAmountRange = 0.5;
 
-  // Movement
-  var horzMove;
-  var vertMove;
-  var moveRange = 40;
-  var movementTime = 1500;
+  // Jitter
+  var jitterX;
+  var jitterY;
+  var jitterTime;
+  var jitterTimeRange = 1500;
+  var jitterRange = 40;
 
   function initCircles() {
-    $circle.each(function(index) {
+    $(".circle").each(function(index) {
       $(this).velocity({
         "left": $(this).attr("data-start-x") + "px",
         "top": $(this).attr("data-start-y") + "px",
@@ -31,56 +26,31 @@
         "opacity": "1",
       }, 1000);
       animateScale(this);
-      animateMovement(this);
+      animateJitter(this);
     });
   }
 
   function animateScale(circle) {
     scaleTime = Math.ceil(Math.random() * (scaleTimeRange / 2) + (scaleTimeRange / 2));
     scaleAmount = Math.random() * scaleAmountRange + 1;
-    $(circle).find(".circle__size").velocity({
+    $(circle).find(".circle__scale").velocity({
       "scale": scaleAmount,
     }, scaleTime, function() {
       animateScale(circle);
     });
   }
 
-  function animateMovement(circle) {
-
-  }
-
-  function update() {
-    return {
-      tS: Math.ceil(Math.random() * (sizeTime / 2) + (sizeTime / 2)),
-      tM: Math.ceil(Math.random() * (movementTime / 2) + (movementTime / 2)),
-      mX: -(moveRange / 2) + (Math.random() * moveRange),
-      mY: -(moveRange / 2) + (Math.random() * moveRange),
-      sA: Math.random() * sizeRange + 1,
-    };
-  }
-
-  /*
-  var sizing = setInterval(function() {
-    $circleSize.each(function(index) {
-      scaleAmount = Math.random() * sizeRange + 1;
-      $(this).velocity({
-        "scale": scaleAmount,
-      }, sizeTime);
+  function animateJitter(circle) {
+    jitterX = -(jitterRange / 2) + (Math.random() * jitterRange);
+    jitterY = -(jitterRange / 2) + (Math.random() * jitterRange);
+    jitterTime = Math.ceil(Math.random() * (jitterTimeRange / 2) + (jitterTimeRange / 2));
+    $(circle).find(".circle__jitter").velocity({
+      "translateX": jitterX,
+      "translateY": jitterY,
+    }, jitterTime, function() {
+      animateJitter(circle);
     });
-  }, sizeTime);
-
-  var movement = setInterval(function() {
-    $circleMove.each(function(index) {
-      horzMove = -(moveRange / 2) + (Math.random() * moveRange);
-      vertMove = -(moveRange / 2) + (Math.random() * moveRange);
-      //console.log("vertMove:", vertMove, "horzMove:", horzMove);
-      $(this).velocity({
-        "translateX": horzMove,
-        "translateY": vertMove,
-      }, movementTime);
-    });
-  }, movementTime);
-  */
+  }
 
   initCircles();
 
